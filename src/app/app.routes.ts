08ea9bd3ef1e3490086aab670d3b89095f1b2573
JaviGuz <javier.guzman@feverup.com>
2019-02-10 21:10:34 +0100

    routas hijas de usuario en archivo usuario.routes e importar al general

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,22 +3,12 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
 import { AppComponent } from './app.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { USUARIO_ROUTES } from './components/usuario/usuario.routes';
+
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent},
-    {
-        path: 'usuario/:id',
-        component: UsuarioComponent,
-        children: [
-            { path: 'nuevo', component: UsuarioNuevoComponent },
-            { path: 'editar', component: UsuarioEditarComponent },
-            { path: 'detalle', component: UsuarioDetalleComponent },
-            { path: '**', pathMatch: 'full', redirectTo: 'nuevo' }
-        ]
-    },
+    { path: 'usuario/:id', component: UsuarioComponent, children: USUARIO_ROUTES},
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
