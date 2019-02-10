64aba56ac845ed56a922c724691521aa56aab85c
JaviGuz <javier.guzman@feverup.com>
2019-02-10 20:50:34 +0100

    Directive component y routes hijas

--- src/app/directives/resaltado.directive.ts
+++ src/app/directives/resaltado.directive.ts
@@ -1,13 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appResaltado]'
 })
 export class ResaltadoDirective {
 
-  constructor() {
+  constructor(private el: ElementRef) {
     console.log('directiva llamada');
+  }
+  @Input('appResaltado') nuevoColor: string; // Declaración de la variable diciendole que viene de fuera
 
+  @HostListener('mouseenter') mouseEntro() {
+    this.resaltar(this.nuevoColor || 'yellow');
+    // this.el.nativeElement.style.backgroundColor = 'yellow';
   }
 
+  @HostListener('mouseleave') mouseSalio() {
+    this.resaltar(null);
+    // this.el.nativeElement.style.backgroundColor = null;
+  }
+
+  private resaltar (color: string) {
+     this.el.nativeElement.style.backgroundColor = color;
+  }
 }
